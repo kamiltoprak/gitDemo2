5134da8071378b71b487c4176c897f4ff1f5e345
kamiltoprak <ktoprak@gmail.com>
2020-08-01 10:32:20 -0400

    new juky31 1

--- src/test1/login.java
+++ src/test1/login.java
@@ -1,4 +1,7 @@
 package test1;
 
 public class login {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
 }
--- src/test1/logout.java
+++ src/test1/logout.java
@@ -1,4 +1,7 @@
 package test1;
 
 public class logout {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
 }
